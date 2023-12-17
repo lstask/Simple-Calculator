e11f497576b1c7cb3e2e790376d9538db3e48a2b
lstask <153090484+lstask@users.noreply.github.com>
2023-12-17 21:41:00 +0300

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,8 @@
+print("Hello, this is a simple calculator!")
+print("Please, enter your numbers:")
+number_1 = int (input())
+number_2 = int (input())
+ask = number_1 + number_2
+print("Your answer is:")
+print(ask)
+print("Thanks for using!")
